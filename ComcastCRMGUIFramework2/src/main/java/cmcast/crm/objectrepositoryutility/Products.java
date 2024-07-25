4d4af7a5ffd210b0bd8b60793e0cedf756d3a409
hp <hp@DESKTOP-EJDRMVG.mshome.net>
2024-07-25 12:48:45 +0530

    Add Search Textfield in POM Class

--- ComcastCRMGUIFramework2/src/main/java/cmcast/crm/objectrepositoryutility/Products.java
+++ ComcastCRMGUIFramework2/src/main/java/cmcast/crm/objectrepositoryutility/Products.java
@@ -17,6 +17,9 @@ public class Products {
 	private WebElement createProductImgBtn;
 	
 	@FindBy(xpath = "(//input[@type='button'])[1]")
+	private WebElement searchTextEle1;
+	
+	@FindBy(xpath = "(//input[@type='button'])[1]")
 	private WebElement searchBtnEle2;
 	
 	public WebElement getCreateProductImgBtn() {
@@ -25,6 +28,10 @@ public class Products {
 
 	public WebElement getSearchBtnEle2() {
 		return searchBtnEle2;
+	}
+
+	public WebElement getSearchTextEle1() {
+		return searchTextEle1;
 	}
 	
 	
